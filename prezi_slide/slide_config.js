4c4388208005baf2e30da47e2fee712469785d5e
Alan Titor <belosianau@gmail.com>
2014-02-14 16:59:16 +0800

    prezi slides finish.

---
+++ prezi_slide/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '輕鬆學會製作精緻投影片',
+    subtitle: '使用Prezi',
+    eventInfo: {
+      title: 'Prezi教學',
+      date: '2014年2月'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/ntou_logo_tiny.gif',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: '何欣諺 陳銘杰 王振碩',
+    company: 'Lab man, 海洋大學資工系嵌入式系統實驗室',
+    //gplus: 'http://plus.google.com/1234567890',
+    //twitter: '@yourhandle',
+    //www: 'http://www.you.com',
+    //github: 'http://github.com/you'
+  }]
+};
+
